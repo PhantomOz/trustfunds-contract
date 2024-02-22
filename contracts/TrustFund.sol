4748dbc4dff24172ce86e3326cbfe743ea7cf752
PhantomOz <faniogor@gmail.com>
2024-02-22 19:36:05 +0100

    This commit creates the vault contract

--- contracts/TrustFund.sol
+++ contracts/TrustFund.sol
@@ -2,11 +2,28 @@
 pragma solidity ^0.8.19;
 
 contract TrustFund {
-    mapping(address => mapping(address => Vault)) public addressToVault;
-    mapping(address => uint256) public balances;
+    mapping(address => mapping(address => Vault)) public donorToVault;
+    mapping(address => Vault) public beneficiaryTobalance;
 
     struct Vault {
         uint256 _amount;
         uint256 _unlock;
+        bool isCreated;
+    }
+
+    function createVault(address _beneficiary, uint256 _unlockTime) external payable{
+        if(_unlockTime < block.timestamp){
+            revert();
+        }
+        if(_beneficiary == address(0)){
+            revert ();
+        }
+        if(msg.value <= 0){
+            revert();
+        }
+        if(donorToVault[msg.sender][_beneficiary].isCreated){
+            revert ();
+        }
+
     }
 }
---
+++ contracts/Vault.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+contract Vault{
+    uint256 private balance;
+    uint256 private unlockTime;
+}
